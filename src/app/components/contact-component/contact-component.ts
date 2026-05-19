8efa04caf3822a8394b00fd21ae23903c99f3a6f
mpessa237 <hervempessa7@gmail.com>
2026-05-19 08:39:23 +0100

    feature:mise en place des des composant about et contact

--- src/app/components/contact-component/contact-component.ts
+++ src/app/components/contact-component/contact-component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-component',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './contact-component.html',
   styleUrl: './contact-component.css',
 })
 export class ContactComponent {
+  contactData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
+
+  onSubmit() {
+    console.log('Ticket de support créé avec succès :', this.contactData);
+
+    alert(`Merci ${this.contactData.name}, votre message a bien été transmis au support SGAE !`);
+
+    this.contactData = {
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    };
+  }
 
 }
